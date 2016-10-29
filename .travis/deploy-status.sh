3cd61d7bd628c0c598e2c31767b72e61a9268be9
Zetok Zalbavar <zetok@openmailbox.org>
2016-10-29 18:11:16 +0100

    Add index.html for deployment, apparently gh-pages don't work without it

--- .travis/deploy-status.sh
+++ .travis/deploy-status.sh
@@ -7,6 +7,7 @@ cd web
 git init
 git config user.name "Travis CI"
 git config user.email "zetok@users.noreply.github.com"
+echo "<body></body>" > index.html
 git add .
 git commit --quiet -m "Deploy to GH pages."
 git push --force --quiet "https://${GH_TOKEN}@github.com/qTox/release-schedule.git" master:gh-pages &> /dev/null
